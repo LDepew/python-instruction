1b5d49ea37503a7a757c9b5f007fa9178421d4f5
Lance <Metal_Death_Music@yahoo.com>
2022-10-17 17:19:39 -0400

    Giraffe Variables

---
+++ gvariables/variables.py
@@ -0,0 +1,11 @@
+character_name = "John"
+character_age = 50.5678213
+is_Male = False
+
+print("There once was a man named " + character_name + ", ")
+print(f"he was {character_age} years old. ")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print(f"but didn't like being {character_age}.")
