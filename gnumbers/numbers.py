069afbe125dcd900cc36c2fb8e9248a46f42e5ff
Lance <Metal_Death_Music@yahoo.com>
2022-10-18 15:26:15 -0400

    CS50 Numbers

---
+++ gnumbers/numbers.py
@@ -0,0 +1,24 @@
+from math import *
+
+print(-2.0987)
+print(3 + 4.5)
+
+print(3 * (4 + 5))
+print(10 % 3)
+
+my_num = 5
+print(str(my_num) + " my favorite number")
+
+my_num2 = -5
+print(abs(my_num2))
+
+print(pow(4, 6))
+
+print(max(4, 6))
+print(min(4, 6))
+
+print(round(3.7))
+
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
