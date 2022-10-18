3f527cc2c8a55b31f960e0011f7af814b725b26e
Lance <Metal_Death_Music@yahoo.com>
2022-10-18 15:12:41 -0400

    CS50 Strings

---
+++ strings/strings.py
@@ -0,0 +1,9 @@
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Elephant"))
+print(len(phrase))
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(phrase[0])
+print(phrase.index("Acad"))
