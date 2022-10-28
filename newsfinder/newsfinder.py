b2d42c07aa4af6a50a49e1c84da9828a26b06450
Lance <Metal_Death_Music@yahoo.com>
2022-10-28 18:02:42 -0400

    News Finder v.2

--- newsfinder/newsfinder.py
+++ newsfinder/newsfinder.py
@@ -1,5 +1,14 @@
+from ast import Import
 import webbrowser
 
-keyword = input("Enter keyword(s) to search: ")
+try:
+    from googlesearch import search
+except ImportError:
+    print("No module named google")
 
-webbrowser.open("https://www.google.com/search?q=" + keyword + "&sxsrf=ALiCzsZNnUtCuzheJ4w8C5qjGrcmyqUd9w%3A1666993162019&ei=CkxcY9dUiNnk2g-e9KOABA&ved=0ahUKEwjXgY-68YP7AhWILFkFHR76CEAQ4dUDCBA&uact=5&oq= + keyword + &gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCC4QsQMQgwEQkQIyBAgAEEMyCAgAELEDEJECMgQIABBDMgQILhBDMgsILhCxAxCDARDUAjIECC4QQzIECAAQQzIECC4QQzIOCC4QgAQQsQMQgwEQ1AI6BAgjECc6BQgAEJECOhEILhCABBCxAxCDARDHARDRAzoFCAAQgAQ6CwguEIAEELEDEIMBOgsILhCABBDHARCvAToFCC4QgAQ6CgguELEDEIMBEEM6CAguELEDEIMBOgsIABCABBCxAxCDAToICAAQsQMQgwE6DgguELEDEIMBEMcBENEDOgoILhDHARDRAxBDOgsILhCABBCxAxDUAjoICC4QgAQQsQNKBAhNGAFKBAhBGABKBAhGGABQAFj-A2DbCGgAcAF4AIABVYgBiQOSAQE1mAEAoAEBwAEB&sclient=gws-wiz-serp")
+query = input("Enter keyword(s) to search: ")
+
+for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+    print(j)
+
+webbrowser.open("https://www.google.com/search?q=" + query + "&sxsrf=ALiCzsZNnUtCuzheJ4w8C5qjGrcmyqUd9w%3A1666993162019&ei=CkxcY9dUiNnk2g-e9KOABA&ved=0ahUKEwjXgY-68YP7AhWILFkFHR76CEAQ4dUDCBA&uact=5&oq=" + query + "&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCC4QsQMQgwEQkQIyBAgAEEMyCAgAELEDEJECMgQIABBDMgQILhBDMgsILhCxAxCDARDUAjIECC4QQzIECAAQQzIECC4QQzIOCC4QgAQQsQMQgwEQ1AI6BAgjECc6BQgAEJECOhEILhCABBCxAxCDARDHARDRAzoFCAAQgAQ6CwguEIAEELEDEIMBOgsILhCABBDHARCvAToFCC4QgAQ6CgguELEDEIMBEEM6CAguELEDEIMBOgsIABCABBCxAxCDAToICAAQsQMQgwE6DgguELEDEIMBEMcBENEDOgoILhDHARDRAxBDOgsILhCABBCxAxDUAjoICC4QgAQQsQNKBAhNGAFKBAhBGABKBAhGGABQAFj-A2DbCGgAcAF4AIABVYgBiQOSAQE1mAEAoAEBwAEB&sclient=gws-wiz-serp")
