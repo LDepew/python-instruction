0e24ab4329565fa482305e2ea4e591d94e5c6e98
Lance <Metal_Death_Music@yahoo.com>
2022-11-01 15:58:55 -0400

    News Finder v.3

--- newsfinder/newsfinder.py
+++ newsfinder/newsfinder.py
@@ -6,9 +6,17 @@ try:
 except ImportError:
     print("No module named google")
 
-query = input("Enter keyword(s) to search: ")
+selection = input("Youtube or Google: ")
 
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    print(j)
+if selection == "Google" or selection == "google":
+    query = input("Enter keyword(s) to search: ")
 
-webbrowser.open("https://www.google.com/search?q=" + query + "&sxsrf=ALiCzsZNnUtCuzheJ4w8C5qjGrcmyqUd9w%3A1666993162019&ei=CkxcY9dUiNnk2g-e9KOABA&ved=0ahUKEwjXgY-68YP7AhWILFkFHR76CEAQ4dUDCBA&uact=5&oq=" + query + "&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCC4QsQMQgwEQkQIyBAgAEEMyCAgAELEDEJECMgQIABBDMgQILhBDMgsILhCxAxCDARDUAjIECC4QQzIECAAQQzIECC4QQzIOCC4QgAQQsQMQgwEQ1AI6BAgjECc6BQgAEJECOhEILhCABBCxAxCDARDHARDRAzoFCAAQgAQ6CwguEIAEELEDEIMBOgsILhCABBDHARCvAToFCC4QgAQ6CgguELEDEIMBEEM6CAguELEDEIMBOgsIABCABBCxAxCDAToICAAQsQMQgwE6DgguELEDEIMBEMcBENEDOgoILhDHARDRAxBDOgsILhCABBCxAxDUAjoICC4QgAQQsQNKBAhNGAFKBAhBGABKBAhGGABQAFj-A2DbCGgAcAF4AIABVYgBiQOSAQE1mAEAoAEBwAEB&sclient=gws-wiz-serp")
+    for j in search(query, tld="co.uk", num=10, stop=10, pause=2):
+        print(j)
+
+    webbrowser.open("https://www.google.com/search?q=" + query + "&sxsrf=ALiCzsZNnUtCuzheJ4w8C5qjGrcmyqUd9w%3A1666993162019&ei=CkxcY9dUiNnk2g-e9KOABA&ved=0ahUKEwjXgY-68YP7AhWILFkFHR76CEAQ4dUDCBA&uact=5&oq=" + query + "&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCC4QsQMQgwEQkQIyBAgAEEMyCAgAELEDEJECMgQIABBDMgQILhBDMgsILhCxAxCDARDUAjIECC4QQzIECAAQQzIECC4QQzIOCC4QgAQQsQMQgwEQ1AI6BAgjECc6BQgAEJECOhEILhCABBCxAxCDARDHARDRAzoFCAAQgAQ6CwguEIAEELEDEIMBOgsILhCABBDHARCvAToFCC4QgAQ6CgguELEDEIMBEEM6CAguELEDEIMBOgsIABCABBCxAxCDAToICAAQsQMQgwE6DgguELEDEIMBEMcBENEDOgoILhDHARDRAxBDOgsILhCABBCxAxDUAjoICC4QgAQQsQNKBAhNGAFKBAhBGABKBAhGGABQAFj-A2DbCGgAcAF4AIABVYgBiQOSAQE1mAEAoAEBwAEB&sclient=gws-wiz-serp")
+
+elif selection == "Youtube" or selection == "youtube":
+    query = input("Enter keyword(s) to search: ")
+
+    webbrowser.open("https://www.youtube.com/results?search_query=" + query)
