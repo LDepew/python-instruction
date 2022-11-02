9836b59640dca9a10eb64113686b2ef570f286ac
Lance <Metal_Death_Music@yahoo.com>
2022-11-02 14:58:24 -0400

    News Finder v.4

--- newsfinder/newsfinder.py
+++ newsfinder/newsfinder.py
@@ -11,10 +11,15 @@ selection = input("Youtube or Google: ")
 if selection == "Google" or selection == "google":
     query = input("Enter keyword(s) to search: ")
 
-    for j in search(query, tld="co.uk", num=10, stop=10, pause=2):
-        print(j)
+    option = input("Open Google or View List: ")
+    if option == "Open Google" or option == "open google":
+        webbrowser.open("https://www.google.com/search?q=" + query + "&sxsrf=ALiCzsZNnUtCuzheJ4w8C5qjGrcmyqUd9w%3A1666993162019&ei=CkxcY9dUiNnk2g-e9KOABA&ved=0ahUKEwjXgY-68YP7AhWILFkFHR76CEAQ4dUDCBA&uact=5&oq=" + query + "&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCC4QsQMQgwEQkQIyBAgAEEMyCAgAELEDEJECMgQIABBDMgQILhBDMgsILhCxAxCDARDUAjIECC4QQzIECAAQQzIECC4QQzIOCC4QgAQQsQMQgwEQ1AI6BAgjECc6BQgAEJECOhEILhCABBCxAxCDARDHARDRAzoFCAAQgAQ6CwguEIAEELEDEIMBOgsILhCABBDHARCvAToFCC4QgAQ6CgguELEDEIMBEEM6CAguELEDEIMBOgsIABCABBCxAxCDAToICAAQsQMQgwE6DgguELEDEIMBEMcBENEDOgoILhDHARDRAxBDOgsILhCABBCxAxDUAjoICC4QgAQQsQNKBAhNGAFKBAhBGABKBAhGGABQAFj-A2DbCGgAcAF4AIABVYgBiQOSAQE1mAEAoAEBwAEB&sclient=gws-wiz-serp")
 
-    webbrowser.open("https://www.google.com/search?q=" + query + "&sxsrf=ALiCzsZNnUtCuzheJ4w8C5qjGrcmyqUd9w%3A1666993162019&ei=CkxcY9dUiNnk2g-e9KOABA&ved=0ahUKEwjXgY-68YP7AhWILFkFHR76CEAQ4dUDCBA&uact=5&oq=" + query + "&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzILCC4QsQMQgwEQkQIyBAgAEEMyCAgAELEDEJECMgQIABBDMgQILhBDMgsILhCxAxCDARDUAjIECC4QQzIECAAQQzIECC4QQzIOCC4QgAQQsQMQgwEQ1AI6BAgjECc6BQgAEJECOhEILhCABBCxAxCDARDHARDRAzoFCAAQgAQ6CwguEIAEELEDEIMBOgsILhCABBDHARCvAToFCC4QgAQ6CgguELEDEIMBEEM6CAguELEDEIMBOgsIABCABBCxAxCDAToICAAQsQMQgwE6DgguELEDEIMBEMcBENEDOgoILhDHARDRAxBDOgsILhCABBCxAxDUAjoICC4QgAQQsQNKBAhNGAFKBAhBGABKBAhGGABQAFj-A2DbCGgAcAF4AIABVYgBiQOSAQE1mAEAoAEBwAEB&sclient=gws-wiz-serp")
+    elif option == "View List" or option == "view list":
+        for j in search(query, tld="co.uk", num=10, stop=10, pause=2):
+            print(j)
+
+    
 
 elif selection == "Youtube" or selection == "youtube":
     query = input("Enter keyword(s) to search: ")
